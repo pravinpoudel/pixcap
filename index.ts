9d4349da8c746f4f20953a9573d1b39889abed7c
pravinpoudel <prvnpoudel4@gmail.com>
2022-12-18 00:04:24 -0700

    menu on box input elem change shape

--- index.ts
+++ index.ts
@@ -28,7 +28,7 @@ interface Position2D {
 let mousePointer = {} as Position2D;
 let highLightLayer: HighlightLayer;
 let camera: ArcRotateCamera;
-let selectedMesh: AbstractMesh;
+let selectedMesh: AbstractMesh | null;
 let sceneObjects: Array<Mesh> = [];
 
 function prepareScene() {
@@ -58,7 +58,9 @@ function prepareScene() {
   const cylinder = MeshBuilder.CreateCylinder("Cylinder", {}, scene);
   cylinder.position.set(2, 0, 0);
   sceneObjects.push(cylinder);
+}
 
+function init() {
   highLightLayer = new HighlightLayer("highlight", scene);
 }
 
@@ -67,10 +69,92 @@ function onPointerMove(e: MouseEvent) {
   mousePointer.y = scene.pointerY;
 }
 
-function clearUI() {}
+function clearUI() {
+  (document.getElementsByClassName("menu-UI")[0] as HTMLElement).style.display =
+    "none";
+}
+
+function showPlaneUI(
+  selectedMesh: AbstractMesh,
+  dimension: { x: number; y: number; z: number }
+) {
+  let mainMenu = document.getElementsByClassName("menu-UI")[0] as HTMLElement;
+  mainMenu.style.display = "block";
+  console.log(mousePointer);
+  mainMenu.style.left = mousePointer.x + "px";
+  mainMenu.style.top = mousePointer.y + "px";
+
+  let attributeDiv = document.getElementById("attributes_div") as HTMLElement;
+  attributeDiv.innerHTML = "";
+  type dimType = keyof typeof dimension;
+  type attrType = keyof typeof selectedMesh.scaling;
+  for (let key in dimension) {
+    let element = `<label for="${key}"> ${key}: </label>  <input type="number" class="dimensionInput" id=${key} value="${
+      dimension[key as dimType]
+    }" min="0.2" max="2.0"  data-attr=${key} > <hr />`;
+    attributeDiv.innerHTML += element;
+  }
+  let inputElements = document.getElementsByClassName("dimensionInput");
+  for (let i = 0; i < inputElements.length; i++) {
+    inputElements[i].addEventListener("input", (event) => {
+      let inputValue = (event.target as HTMLInputElement).value;
+      let value = Math.min(2.0, +inputValue) as number;
+      value = Math.max(0.2, value);
+      let changedAttr = (event.target as HTMLInputElement).dataset.attr;
+      console.log(selectedMesh.scaling);
+      if (changedAttr == "x") {
+        selectedMesh.scaling.x = value;
+        selectedMesh.computeWorldMatrix();
+      }
+      if (changedAttr == "y") {
+        selectedMesh.scaling.y = value;
+        selectedMesh.computeWorldMatrix();
+      }
+
+      if (changedAttr == "z") {
+        selectedMesh.scaling.z = value;
+        selectedMesh.computeWorldMatrix();
+      }
+    });
+  }
+}
+
+function showSphereUI(radius: number) {
+  console.log(radius);
+}
+
+function showCylinderUI(height: number, radius: number) {
+  console.log(height, radius);
+}
 
 function showUI(selectedMesh: Mesh) {
-  selectedMesh.scaling.x = 2;
+  let name = selectedMesh.name;
+  name = name.toLowerCase();
+  switch (name) {
+    case "plane":
+      var boundingBox = selectedMesh.getBoundingInfo().boundingBox;
+      let dimension = {
+        x: boundingBox.maximum.x - boundingBox.minimum.x,
+        y: boundingBox.maximum.y - boundingBox.minimum.y,
+        z: boundingBox.maximum.z - boundingBox.minimum.y,
+      };
+      console.log(dimension);
+      showPlaneUI(selectedMesh, dimension);
+      break;
+    case "icosphere":
+      let radius = selectedMesh.getBoundingInfo().boundingSphere.radius;
+      showSphereUI(radius);
+      break;
+    case "cylinder":
+      var boundingBox = selectedMesh.getBoundingInfo().boundingBox;
+      let height = boundingBox.maximum.y - boundingBox.minimum.y;
+      let cyRadius = boundingBox.maximum.x;
+      showCylinderUI(height, cyRadius);
+      break;
+    default:
+      console.log("name is not detected");
+  }
+
   selectedMesh.computeWorldMatrix();
 }
 
@@ -84,6 +168,7 @@ function selectMesh() {
   );
   //  right now we only have three mesh in scene
   if (intersection?.hit && intersection.pickedMesh) {
+    // check if selected elements are expected elements
     if (selectedMesh != intersection.pickedMesh) {
       console.log("selected for first time");
       selectedMesh = intersection.pickedMesh;
@@ -93,11 +178,13 @@ function selectMesh() {
     }
   } else {
     highLightLayer.removeAllMeshes();
+    selectedMesh = null;
     clearUI();
   }
 }
 
 prepareScene();
+init();
 
 engine.runRenderLoop(() => {
   scene.render();
